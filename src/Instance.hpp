85514ba6ab4fc56547206211183cd193c33a9728
Matei Istoan <mistoan@scm.gforge.inria.fr>
2015-06-29 14:45:00 +0200

    added Instance

--- src/Instance.hpp
+++ src/Instance.hpp
@@ -17,66 +17,98 @@ using namespace std;
 
 namespace flopoco {
 
-class Instance {
+	class Instance {
 
-public:
+	public:
 
-	/**
-	 * Return the name of the operator
-	 */
-	std::string getName();
+		/**
+		 * Instance constructor
+		 */
+		Instance(std::string name, Operator* op);
 
-	/**
-	 * Set the name of the operator
-	 */
-	void setName();
+		/**
+		 * Instance constructor
+		 */
+		Instance(std::string name, Operator* op, map<std::string, Signal*> inPortMap, map<std::string, Signal*> outPortMap);
 
-	/**
-	 * Return the the operator for this instance
-	 */
-	std::string getOperator();
-
-	/**
-	 * Add a new input port connection
-	 */
-	std::string addInputPort(std::string portName, Signal* connectedSignal);
-
-	/**
-	 * Remove an input port connection
-	 */
-	bool removeInputPort(std::string portName);
-
-	/**
-	 * Clear the input port connections
-	 */
-	void clearInputPorts();
-
-	/**
-	 * Add a new output port connection
-	 */
-	std::string addOutputPort(std::string portName, Signal* connectedSignal);
-
-	/**
-	 * Remove an output port connection
-	 */
-	bool removeOutputPort(std::string portName);
-
-	/**
-	 * Clear the output port connections
-	 */
-	void clearOutputPorts();
+		/**
+		 * Instance constructor
+		 */
+		~Instance();
 
 
-	std::string name;                           /*< the name of the instance */
-	Operator* op;                               /*< the operator for this instance */
-	map<std::string, Signal*> inPortMap;        /*< the port mapping for the inputs of the operator */
-	map<std::string, Signal*> outPortMap;       /*< the port mapping for the outputs of the operator */
+		/**
+		 * Return the name of the operator
+		 */
+		std::string getName();
 
-protected:
+		/**
+		 * Set the name of the operator
+		 */
+		void setName(std::string name);
 
-private:
+		/**
+		 * Return the the operator for this instance
+		 */
+		Operator* getOperator();
 
-};
+		/**
+		 * Set the the operator for this instance
+		 */
+		void setOperator(Operator* op);
+
+		/**
+		 * Add a new input port connection
+		 */
+		void addInputPort(std::string portName, Signal* connectedSignal);
+
+		/**
+		 * Add a set of new input port connections
+		 */
+		void addInputPorts(map<std::string, Signal*> signals);
+
+		/**
+		 * Remove an input port connection
+		 */
+		void removeInputPort(std::string portName);
+
+		/**
+		 * Clear the input port connections
+		 */
+		void clearInputPorts();
+
+		/**
+		 * Add a new output port connection
+		 */
+		void addOutputPort(std::string portName, Signal* connectedSignal);
+
+		/**
+		 * Add a set of new output port connections
+		 */
+		void addOutputPorts(map<std::string, Signal*> signals);
+
+		/**
+		 * Remove an output port connection
+		 */
+		void removeOutputPort(std::string portName);
+
+		/**
+		 * Clear the output port connections
+		 */
+		void clearOutputPorts();
+
+
+	protected:
+
+		std::string name;                           /*< the name of the instance */
+		Operator* op;                               /*< the operator for this instance */
+		map<std::string, Signal*> inPortMap;        /*< the port mapping for the inputs of the operator */
+		map<std::string, Signal*> outPortMap;       /*< the port mapping for the outputs of the operator */
+
+
+	private:
+
+	};
 
 } //namespace
 
