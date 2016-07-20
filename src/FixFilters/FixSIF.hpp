a55a483616707261d39c902a03f1c8a95981053f
Matei Istoan <matei.istoan@inria.fr>
2016-07-20 17:01:19 +0200

    added the variables to the SIF.

--- src/FixFilters/FixSIF.hpp
+++ src/FixFilters/FixSIF.hpp
@@ -47,10 +47,21 @@ namespace flopoco{
 		static void registerFactory();
 
 	private:
+		int n_u;                     /**< the size of the u vector (the number of inputs) */
+		int n_t;                     /**< the size of the t vector (the number of temporary variables) */
+		int n_x;                     /**< the size of the x vector (the number of states) */
+		int n_y;                     /**< the size of the y vector (the number of outputs) */
 
+		vector<int> m_u;             /**< the vector of MSBs of each u */
+		vector<int> l_u;             /**< the vector of LSBs of each u */
+		vector<int> m_t;             /**< the vector of MSBs of each t */
+		vector<int> l_t;             /**< the vector of LSBs of each t */
+		vector<int> m_x;             /**< the vector of MSBs of each x */
+		vector<int> l_x;             /**< the vector of LSBs of each x */
+		vector<int> m_y;             /**< the vector of MSBs of each y */
+		vector<int> l_y;             /**< the vector of LSBs of each y */
 
-	private:
-
+		vector<vector<string>> Z;    /**< the matrix of coefficients Z */
 
 	};
 
