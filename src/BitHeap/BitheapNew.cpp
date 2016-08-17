448fda2247fb9bf5d22066dd3bfe002cb40b3fab
Matei Istoan <matei.istoan@inria.fr>
2016-08-17 20:08:22 +0200

    added addUnsignedBitVector functions to the new bitheap.

--- src/BitHeap/BitheapNew.cpp
+++ src/BitHeap/BitheapNew.cpp
@@ -450,6 +450,69 @@ namespace flopoco {
 	}
 
 
+	void BitheapNew::addUnsignedBitVector(string signalName, unsigned size, int weight)
+	{
+		if(isFixedPoint && ((weight < lsb) || (weight > msb)))
+			THROWERROR("Weight (=" << weight << ") out of bitheap bit range in removeBit");
+		if(!isFixedPoint && ((weight < 0) || (weight >= size)))
+			THROWERROR("Weight (=" << weight << ") out of bitheap bit range in removeBit");
+
+		int startIndex, endIndex;
+
+		if(weight < lsb)
+			startIndex = lsb;
+		else
+			startIndex = weight;
+		if(size+weight > msb)
+			endIndex = msb;
+		else
+			endIndex = size+weight-1;
+
+		for(int i=startIndex; i<=endIndex; i++)
+			addBit(i, join(signalName, of(i-startIndex+startIndex-weight)));
+	}
+
+
+	void BitheapNew::addUnsignedBitVector(string signalName, int msb, int lsb, int weight)
+	{
+		if(isFixedPoint && ((weight < lsb) || (weight > msb)))
+			THROWERROR("Weight (=" << weight << ") out of bitheap bit range in removeBit");
+		if(!isFixedPoint && ((weight < 0) || (weight >= size)))
+			THROWERROR("Weight (=" << weight << ") out of bitheap bit range in removeBit");
+
+		int startIndex, endIndex;
+
+		if(weight+lsb < this->lsb)
+			startIndex = this->lsb;
+		else
+			startIndex = weight+lsb;
+		if(msb+weight > this->msb)
+			endIndex = this->msb;
+		else
+			endIndex = msb+weight;
+
+		for(int i=startIndex; i<=endIndex; i++)
+			addBit(i, join(signalName, of(i-startIndex+startIndex-weight-lsb)));
+	}
+
+
+	void BitheapNew::addUnsignedBitVector(Signal* signal, int weight)
+	{
+		if(signal->isFix())
+			addUnsignedBitVector(signal->getName(), signal->MSB(), signal->LSB(), weight);
+		else
+			addUnsignedBitVector(signal->getName(), signal->width(), weight);
+	}
+
+
+	void BitheapNew::addUnsignedBitVector(Signal* signal, int msb, int lsb, int weight)
+	{
+		if(signal->isFix())
+			addUnsignedBitVector(signal->getName(), msb, lsb, weight);
+		else
+			addUnsignedBitVector(signal->getName(), msb-lsb+1, weight);
+	}
+
 
 
 } /* namespace flopoco */
