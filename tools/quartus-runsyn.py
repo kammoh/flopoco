68ba403f5e6327e4d6f254c1e20b290dec575f36
fdedinec <Florent.de-Dinechin@insa-lyon.fr>
2016-08-29 18:26:37 +0200

    fixed bug in entity option

--- tools/quartus-runsyn.py
+++ tools/quartus-runsyn.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     if (options.entity==None):
         entity = get_last_entity(filename)
     else:
-        entityname=options.entity
+        entity=options.entity
         
     if (options.target==None):
         target = "StratixV"
