4c1d67b1392c0e1798ff72ef32d7d9e2a0c98f1f
Antonin Dudermel <antonin.dudermel@ens-lyon.fr>
2018-07-04 15:34:25 +0200

    comments now allowed in the file src/Factories/Interfaced.txt

--- src/Factories/MakeFactory.sh
+++ src/Factories/MakeFactory.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 #Such Inception
-
+#First arg must contain the path to ./ from the caller
 cd $1
-sed "s/.*/	DEF(\0)\;/" Interfaced.txt > headers.txt
-# elt -> DEF(elt);
-sed "s/.*/			\0::registerFactory()\;/" Interfaced.txt > registration.txt
+../../tools/EraseComments.sh Interfaced.txt InterfacedNoComments.txt
 
+sed "s/.*/	DEF(\0)\;/" InterfacedNoComments.txt > headers.txt
+# elt -> DEF(elt);
+sed "s/.*/			\0::registerFactory()\;/" InterfacedNoComments.txt > registration.txt
 cat head.txt headers.txt middle.txt registration.txt tail.txt > ../Factories.cpp
 
-rm headers.txt registration.txt
+rm headers.txt registration.txt InterfacedNoComments.txt
