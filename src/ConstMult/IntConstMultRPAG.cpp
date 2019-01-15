08f75319937f5753447b8cc31a07c01d63efbe8e
Martin Kumm <kumm@uni-kassel.de>
2019-01-15 16:47:07 +0100

    Added operator for ConstMultAddShift using the RPAG library

---
+++ src/ConstMult/IntConstMultRPAG.cpp
@@ -0,0 +1,130 @@
+/*
+  Integer constant multiplication using minimum number of adders due to
+
+  Gustafsson, O., Dempster, A., Johansson, K., Macleod, M., & Wanhammar, L. (2006).
+  Simplified Design of Constant Coefficient Multipliers. Circuits, Systems, and Signal Processing, 25(2), 225–251.
+
+  All constants up to 19 bit will be realized optimal using precomputed tables provided by the SPIRAL project (http://spiral.ece.cmu.edu/mcm/).
+  
+  Author : Martin Kumm kumm@uni-kassel.de, (emulate adapted from Florent de Dinechin, Florent.de.Dinechin@ens-lyon.fr)
+
+  All rights reserved.
+
+*/
+#if defined(HAVE_PAGLIB) && defined(HAVE_RPAGLIB)
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <gmp.h>
+#include <mpfr.h>
+#include <gmpxx.h>
+#include "../utils.hpp"
+#include "../Operator.hpp"
+
+#include "IntConstMultRPAG.hpp"
+
+#include "pagsuite/scm_solutions.hpp"
+#include "pagsuite/pagexponents.hpp"
+#include "pagsuite/compute_successor_set.h"
+#include "pagsuite/log2_64.h"
+#include "pagsuite/fundamental.h"
+
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+using namespace PAGSuite;
+
+namespace flopoco{
+
+    IntConstMultRPAG::IntConstMultRPAG(Operator* parentOp, Target* target, int wIn, int coeff, bool syncInOut)  : IntConstMultShiftAdd(parentOp, target, wIn, "", false, syncInOut, 1000, false)
+    {
+/*
+        this->coeff = coeff;
+
+        cout << "wIn=" << wIn << ", coeff=" << coeff << endl;
+
+
+        if(coeff <= 0)
+        {
+		  cerr << "Error: Value of constant must be positive and non-zero" << endl;
+          exit(-1);
+        }
+
+        int shift;
+        int coeffOdd = fundamental(coeff, &shift);
+
+        if((coeffOdd < MAX_SCM_CONST) && (coeffOdd > 1))
+        {
+          buildAdderGraph(coeffOdd);
+        }
+        else
+        {
+		  cerr << "Error: (Odd) value of constant must be less than " << MAX_SCM_CONST << " (is " << coeffOdd << ")" << endl;
+          exit(-1);
+        }
+
+        if(coeff != coeffOdd)
+        {
+            adderGraph << "{'O',[" << coeff << "],1,[" << coeffOdd << "],0," << shift << ",0,0,0},";
+        }
+
+        stringstream adderGraphComplete;
+        adderGraphComplete << "{" << adderGraph.str().substr(0,adderGraph.str().length()-1) << "}";
+
+        REPORT(
+                    DEBUG,
+                    "adder_graph=" << adderGraphComplete.str()
+                    );
+
+		ProcessIntConstMultShiftAdd(target,adderGraphComplete.str());
+*/
+		rpag_pointer *rpagp = NULL;
+		rpagp = new rpag(); //default is RPAG with 2 input adders
+		rpagp->target_vec.push_back("123");
+		PAGSuite::cost_model_t cost_model = PAGSuite::HL_FPGA;// with default value
+		PAGSuite::global_verbose = 10;
+		//rpagp->cost_model = cost_model;
+		((rpag_base<vec_t>*)rpagp)->set_cost_model(cost_model);
+		cout << "rpagp->filename=" << rpagp->filename << endl;
+		rpagp->optimize();
+
+        ostringstream name;
+        name << "IntConstMultRPAG_" << coeff << "_" << wIn;
+        setName(name.str());
+    }
+
+    OperatorPtr flopoco::IntConstMultRPAG::parseArguments(OperatorPtr parentOp, Target *target, vector<string> &args ) {
+        int wIn, constant;
+
+        UserInterface::parseInt( args, "wIn", &wIn );
+        UserInterface::parseInt( args, "constant", &constant );
+
+        return new IntConstMultRPAG(parentOp, target, wIn, constant, false);
+    }
+
+    void flopoco::IntConstMultRPAG::registerFactory() {
+
+        UserInterface::add( "IntConstMultRPAG", // name
+                            "Integer constant multiplication using shift and add in an optimal way (i.e., with minimum number of adders). Works for coefficients up to " + std::to_string(MAX_SCM_CONST) + " (19 bit)", // description, string
+                            "ConstMultDiv", // category, from the list defined in UserInterface.cpp
+                            "", //seeAlso
+                            "wIn(int): Input word size; \
+                            constant(int): constant;",
+                            "Nope.",
+                            IntConstMultRPAG::parseArguments
+                          ) ;
+    }
+
+}
+#else
+
+#include "IntConstMultRPAG.hpp"
+namespace flopoco
+{
+	void IntConstMultRPAG::registerFactory() { }
+}
+
+#endif //defined(HAVE_PAGLIB) && defined(HAVE_RPAGLIB)
+
---
+++ src/ConstMult/IntConstMultRPAG.hpp
@@ -0,0 +1,50 @@
+#ifndef INTCONSTMULTOPT_HPP
+#define INTCONSTMULTOPT_HPP
+
+#if defined(HAVE_PAGLIB) && defined(HAVE_RPAGLIB)
+
+#include <sstream>
+#include <gmp.h>
+#include <mpfr.h>
+#include <gmpxx.h>
+#include <cstdlib>
+
+#include "../Operator.hpp"
+#include "IntConstMultShiftAdd.hpp"
+#include "pagsuite/rpag.h"
+
+/**
+	Integer constant multiplication using minimum number of adders due to
+
+	Gustafsson, O., Dempster, A., Johansson, K., Macleod, M., & Wanhammar, L. (2006).
+	Simplified Design of Constant Coefficient Multipliers. Circuits, Systems, and Signal Processing, 25(2), 225–251.
+
+
+	All constants up to 19 bit will be realized optimal using precomputed tables provided by the SPIRAL project (http://spiral.ece.cmu.edu/mcm/).
+
+*/
+
+
+namespace flopoco{
+
+    class IntConstMultRPAG : public IntConstMultShiftAdd
+	{
+	public:
+		/** The standard constructor, inputs the number to implement */ 
+        IntConstMultRPAG(Operator* parentOp, Target* target, int wIn, int c, bool syncInOut=true);
+
+        static OperatorPtr parseArguments(OperatorPtr parentOp, Target *target, vector<string> &args );
+        static void registerFactory();
+	private:
+        stringstream adderGraph;
+#else
+namespace flopoco{
+	class IntConstMultRPAG
+	{
+	public:
+		static void registerFactory();
+#endif //defined(HAVE_PAGLIB) && defined(HAVE_RPAGLIB)
+	};
+}
+
+#endif
