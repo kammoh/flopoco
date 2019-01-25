7745f2da6d0f50206b6030a5e1f34d647401211c
Luc Forget <luc.forget@insa-lyon.fr>
2019-01-25 16:04:40 +0100

    Fix vector default initialisation in splitNonOverlap adder_cost IntConstMultShiftAdd

--- tests/ConstMult/testNodeCost.cpp
+++ tests/ConstMult/testNodeCost.cpp
@@ -23,3 +23,22 @@ BOOST_AUTO_TEST_CASE(SignedShiftedAdd) {
 	BOOST_REQUIRE_MESSAGE(adder_cost == 18 , "AdderCost for graph should be 18, got " <<
 			adder_cost << "instead.");
 }
+
+BOOST_AUTO_TEST_CASE(UnsignedOverlappingAdd) {
+	adder_graph_t adder_graph;
+	string graph_descr = "{{'A',[17],1,[1],0,0,[1],0,4},{'O',[17],1,[17],1,0}}";
+
+	adder_graph.parse_to_graph(graph_descr);
+
+	int adder_cost = IntConstMultShiftAdd_TYPES::getGraphAdderCost(
+			adder_graph,
+			2,
+			true	
+		);
+	
+	BOOST_REQUIRE_MESSAGE(
+			adder_cost == 0, 
+			"AdderCost Non overlapping unsigned addition should be 0, got " <<
+				adder_cost << "instead."
+		);
+}
