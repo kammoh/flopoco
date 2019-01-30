6e8db62881097d37fbd22dbe2de7d5af1e4f52da
Luc Forget <luc.forget@insa-lyon.fr>
2019-01-30 18:38:08 +0100

    Error

--- src/ConstMult/error_comp_graph.cpp
+++ src/ConstMult/error_comp_graph.cpp
@@ -1,6 +1,8 @@
 #include "error_comp_graph.hpp"
 
 #include<set>
+#include <gmpxx.h>
+#include <numeric>
 
 using namespace PAGSuite;
 
@@ -107,5 +109,179 @@ namespace IntConstMultShiftAdd_TYPES {
 		print_aligned_word_graph(adder_graph,truncationReg,input_word_size,output_stream);
 	}
 
+	void df_fill_error(
+			adder_graph_base_node_t* base_node,
+			TruncationRegister const & truncReg,
+			set<adder_graph_base_node_t*>& visited,
+			map<adder_graph_base_node_t*, ErrorStorage> errors,
+			map<adder_graph_base_node_t*, int> propagated_zeros
+		)
+	{
+		if (visited.count(base_node) > 0) {
+			return;
+		}
+		if (is_a<input_node_t>(*base_node)) {
+			errors.insert(make_pair(base_node, ErrorStorage{0, 0}));
+			propagated_zeros.insert(make_pair(base_node, 0));
+			visited.insert(base_node);
+			return;
+		}
+		
+		if (is_a<register_node_t>(*base_node) || is_a<output_node_t>(*base_node)) {
+			register_node_t* t = (register_node_t*) base_node;
+			if (visited.count(t->input) < 1) {
+				df_fill_error(t->input, truncReg, visited, errors, propagated_zeros);
+			}
+			errors.insert(make_pair(base_node, ErrorStorage{0, 0}));
+			int input_zero = (propagated_zeros.find(t->input))->second;
+			propagated_zeros.insert(make_pair(base_node, input_zero + t->input_shift));
+			visited.insert(base_node);
+			return;
+		}
+
+		if (not is_a<adder_subtractor_node_t>(*base_node)) {
+			throw string("IntConstMultShiftAdd_TYPES::get_node_error_ulp : "
+					"Don't know how to handle this type yet");
+		}
+
+		adder_subtractor_node_t* addsub = (adder_subtractor_node_t*) base_node;
+		int min_kz = numeric_limits<int>::max();
+		size_t i = 0;
+		auto trunc = truncReg.getTruncationFor(
+				addsub->output_factor[0][0],
+				addsub->stage
+			);
+
+		ErrorStorage error{0,0};
+
+		for (auto inputNodePtr : addsub->inputs) {
+			if (visited.count(inputNodePtr) < 1) {
+				df_fill_error(
+						inputNodePtr, 
+						truncReg, 
+						visited, 
+						errors, 
+						propagated_zeros
+					);
+			}
+
+			int kz_i = (propagated_zeros.find(inputNodePtr))->second;
+			int truncation_i = trunc[i];
+			int64_t shift_i = addsub->input_shifts[i];
+			int real_loss = max(truncation_i - kz_i, 0);
+			mpz_class loss_i = ((mpz_class{1} << real_loss) - 1) << shift_i;
+			if (addsub->input_is_negative[i]) {
+				error.negative_error += loss_i;
+			} else {
+				error.positive_error += loss_i;
+			}
+			++i;
+
+			int cur_kz = max(kz_i, truncation_i) + shift_i;
+							
+			if (cur_kz < min_kz) {
+				min_kz = cur_kz;
+			}
+		}
+		
+		propagated_zeros.insert(make_pair(base_node, min_kz));
+		errors.insert(make_pair(base_node, error));
+		visited.insert(base_node);
+	}
+
+	void df_accumulate_error(
+			adder_graph_base_node_t* base_node,
+			TruncationRegister const & truncReg,
+			set<adder_graph_base_node_t*>& visited,
+			map<adder_graph_base_node_t*, ErrorStorage> errors,
+			map<adder_graph_base_node_t*, int> propagated_zeros,
+			ErrorStorage& tot_error
+		)
+	{
+		if (visited.count(base_node) < 1) {
+			df_fill_error(base_node, truncReg, visited, errors, propagated_zeros);
+		}
+
+		auto& cur_node_error = (errors.find(base_node))->second;
+		tot_error += cur_node_error;
+
+		if (is_a<register_node_t>(*base_node) || is_a<output_node_t>(*base_node)) {
+			auto t = (register_node_t*) base_node;
+			ErrorStorage sub_tot_error{0,0};
+			df_accumulate_error(
+					t->input, 
+					truncReg, 
+					visited, 
+					errors,
+					propagated_zeros, 
+					sub_tot_error
+				);
+			sub_tot_error.shift(t->input_shift);			
+			tot_error += sub_tot_error;
+		} else if (is_a<adder_subtractor_node_t>(*base_node)) {
+			auto t = (adder_subtractor_node_t*) base_node;
+			size_t nb_inputs = t->inputs.size();
+			ErrorStorage sub_tot{0, 0};
+			int neg_shift = (t->input_shifts[0] < 0) ? -1 * t->input_shifts[0] : 0;
+			for (size_t i = 0 ; i < nb_inputs ; ++i) {
+				ErrorStorage tmp{0, 0};
+				int  cur_shift = max(t->input_shifts[i], 0);
+				df_accumulate_error(
+						t->inputs[i],
+						truncReg,
+						visited,
+						errors,
+						propagated_zeros,
+						tmp
+					);
+				tmp.shift(cur_shift);
+				sub_tot += tmp;
+			}
+			if (neg_shift > 0) {
+				sub_tot.shift(-neg_shift);
+			}
+			
+			tot_error += sub_tot;
+		}
+	}
+
+	ErrorStorage getErrorForNode(
+			adder_graph_base_node_t* base_node,
+			TruncationRegister truncReg
+		)
+	{
+		ErrorStorage ret{0,0};
+		set<adder_graph_base_node_t*> visited;
+		map<adder_graph_base_node_t*, ErrorStorage> errors;
+		map<adder_graph_base_node_t*, int> propagated_zeros;
+		df_accumulate_error(
+				base_node, 
+				truncReg,
+				visited,
+				errors,
+				propagated_zeros,
+				ret
+			);
+		return ret;
+	}
+
+	ErrorStorage ErrorStorage::shift(int shift)
+	{
+		if (shift > 0) {
+			positive_error <<= shift;
+			negative_error <<= shift;
+		} else {
+			positive_error /= (mpz_class{1} << -shift);
+			negative_error /= (mpz_class{1} << -shift);
+		}
+		 return *this;
+	}
+
+	ErrorStorage& ErrorStorage::operator+=(ErrorStorage const & rhs)
+	{
+		positive_error += rhs.positive_error;
+		negative_error += rhs.negative_error;
+		return *this;
+	}
 }
 
--- src/ConstMult/error_comp_graph.hpp
+++ src/ConstMult/error_comp_graph.hpp
@@ -2,29 +2,63 @@
 #define ERROR_COMP_GRAPH_HPP
 
 #include <iostream>
+#include <map>
+#include <set>
+
 #include <pagsuite/adder_graph.h>
 #include "IntConstMultShiftAddTypes.hpp"
 
 using namespace std;
+using namespace PAGSuite;
 
 namespace IntConstMultShiftAdd_TYPES {
+	struct ErrorStorage {
+			mpz_class positive_error;
+			mpz_class negative_error;
+
+			ErrorStorage shift(int shift);
+
+			ErrorStorage& operator+=(ErrorStorage const & rhs);
+	};
 
 	void print_aligned_word_graph(
-			PAGSuite::adder_graph_t & adder_graph,
+			adder_graph_t & adder_graph,
 			TruncationRegister truncationReg,
 			int input_word_size,
 			ostream & output_stream
 	);
 
 	void print_aligned_word_graph(
-			PAGSuite::adder_graph_t & adder_graph,
+			adder_graph_t & adder_graph,
 			string truncations,
 			int input_word_size,
 			ostream & output_stream
 	);
 
+	void df_fill_error(
+			adder_graph_base_node_t* base_node,
+			TruncationRegister const & truncReg,
+			set<adder_graph_base_node_t*>& visited,
+			map<adder_graph_base_node_t*, ErrorStorage> errors,
+			map<adder_graph_base_node_t*, int> propagated_zeros
+		);
+
+	void df_accumulate_error(
+			adder_graph_base_node_t* base_node,
+			TruncationRegister const & truncReg,
+			set<adder_graph_base_node_t*>& visited,
+			map<adder_graph_base_node_t*, ErrorStorage> errors,
+			map<adder_graph_base_node_t*, int> propagated_zeros,
+			ErrorStorage& tot_error
+		);
+
+	ErrorStorage getErrorForNode(
+			adder_graph_base_node_t* base_node,
+			TruncationRegister truncReg
+		);
+
 	void print_aligned_word_node(
-		PAGSuite::adder_graph_base_node_t* node,
+		adder_graph_base_node_t* node,
 		TruncationRegister const & truncationReg,
 		int right_shift,
 		int total_word_size,
@@ -32,6 +66,11 @@ namespace IntConstMultShiftAdd_TYPES {
 		int  truncation,
 		ostream& output_stream
 		);
+
+	ErrorStorage get_node_error_ulp(
+		adder_graph_base_node_t* node,
+		TruncationRegister const & truncReg
+	);
 }
 
 #endif
