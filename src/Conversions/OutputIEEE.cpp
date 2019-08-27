9846bc85f17c6fad41fac0e0e9d1402949ade3ad
Martin Kumm <kumm@uni-kassel.de>
2019-08-27 14:28:39 +0200

    Extensions of OutputIEEE for wEI < wEO

--- src/Conversions/OutputIEEE.cpp
+++ src/Conversions/OutputIEEE.cpp
@@ -49,29 +49,43 @@ namespace flopoco{
 		addFPInput ("X", wEI, wFI);
 		addOutput("R", wEO+wFO+1);
 
-		vhdl << tab << declare("expX", wEI) << "  <= X" << range(wEI+wFI-1, wFI) << ";" << endl;
 		vhdl << tab << declare("fracX", wFI) << "  <= X" << range(wFI-1, 0) << ";" << endl;
 		vhdl << tab << declare("exnX",2) << "  <= X" << range(wEI+wFI+2, wEI+wFI+1) << ";" << endl;
+
+		if(wEI==wEO){
+			vhdl << tab << declare("expX", wEI) << "  <= X" << range(wEI+wFI-1, wFI) << ";" << endl;
+		} else if(wEI<wEO) {
+
+		/* Now, we have to adjust the exponent to a wider word size and a new bias.
+		 * For that, we have to compute Enew = Eold + biasCorr with biasCorr = biasO - biasI where biasI and bias= are the input and output biases (biasI = (1<<(wEI-1))-1, biasO = (1<<(wEO-1))-1)
+		 * This could be done by this line:
+		 *   vhdl << tab << declare(getTarget()->adderDelay(wEO),"expX", wEO) << "  <= (" << zg(wEO-wEI) << " & X" << range(wEI+wFI-1, wFI) << ") + conv_std_logic_vector(" << biasCorr << "," << wEO << ");" << endl;
+		 * Instead, as the wEI-1 LSBs are always zero and all the upper bits are one in biasCorr, we can utilize this to perform this constant addition by simple negations:
+*/
+			vhdl << tab << declare(getTarget()->lutDelay(), "expX", wEO) << "  <= X" << of(wEI+wFI-1);
+			for(int i=0; i < wEO-wEI; i++)
+			{
+				vhdl << " & not X" << of(wEI+wFI-1);
+			}
+			vhdl << " & X" << range(wEI+wFI-2, wFI) << ";" << endl;
+		}
+
 		if(onlyPositiveZeroes)
 		        vhdl << tab << declare(getTarget()->lutDelay(), "sX") << "  <= X(" << wEI+wFI << ") when (exnX = \"01\" or exnX = \"10\") else '0';" << endl;
 		else
 		        vhdl << tab << declare(getTarget()->lutDelay(), "sX") << "  <= X(" << wEI+wFI << ") when (exnX = \"01\" or exnX = \"10\" or exnX = \"00\") else '0';" << endl;
 		vhdl << tab << declare(getTarget()->lutDelay(), "expZero") << "  <= '1' when expX = " << rangeAssign(wEI-1,0, "'0'") << " else '0';" << endl;
-		if(wEI==wEO){ 
+
+		if((wEI==wEO) || (wEI < wEO)){ //TODO Subnormal inputs need to be normalized in case wEI < wEO
 			vhdl << tab << "-- since we have one more exponent value than IEEE (field 0...0, value emin-1)," << endl 
 				  << tab << "-- we can represent subnormal numbers whose mantissa field begins with a 1" << endl;
 
 			if(wFO>=wFI){
-				vhdl << tab << declare(getTarget()->lutDelay(), "sfracX",wFI) << " <= " << endl
-						<< tab << tab << rangeAssign(wFI-1,0, "'0'") << " when (exnX = \"00\") else" << endl //zero
-						<< tab << tab << "'1' & fracX" << range(wFI-1,1) << " when (expZero = '1' and exnX = \"01\") else" << endl  //subnormal, no rounding is performed here
-						<< tab << tab << "fracX when (exnX = \"01\") else " << endl //normal number
-						<< tab << tab << rangeAssign(wFI-1,1, "'0'") << " & exnX(0);" << endl; //+/- infty or NaN
-				vhdl << tab << declare("fracR",wFO) << " <= " << "sfracX";
-				if(wFO>wFI) // need to pad with 0s
-					vhdl << " & CONV_STD_LOGIC_VECTOR(0," << wFO-wFI <<");" << endl;
-				else 
-					vhdl << ";" << endl;
+				vhdl << tab << declare(getTarget()->lutDelay(), "fracR",wFO) << " <= " << endl
+						<< tab << tab << zg(wFO) << " when (exnX = \"00\") else" << endl //zero
+						<< tab << tab << "'1' & fracX" << range(wFI-1, wFO>wFI?0:1) << " & " << zg(wFO-wFI-(wFO>wFI?1:0)) << " when (expZero = '1' and exnX = \"01\") else" << endl  //subnormal, no rounding is performed here when wFO==wFI
+						<< tab << tab << "fracX " << " & " << zg(wFO-wFI) << " when (exnX = \"01\") else " << endl //normal number
+						<< tab << tab << zg(wFO-1) << " & exnX(0);" << endl; //+/- infty or NaN
 				vhdl << tab << declare(getTarget()->lutDelay(), "expR",wEO) << " <=  " << endl
 						<< tab << tab << rangeAssign(wEO-1,0, "'0'") << " when (exnX = \"00\") else" << endl
 						<< tab << tab << "expX when (exnX = \"01\") else " << endl
@@ -108,16 +122,6 @@ namespace flopoco{
 			}
 
 		}
-
-
-
-		else if (wEI<wEO) { // No overflow possible. Subnormal inputs need to be normalized
-			throw  string("OutputIEEE not yet implemented for wEI<wEO, send us a mail if you need it");
-			// cout << "Warning: subnormal inputs would be representable in the destination format,\n   but will be flushed to zero anyway (TODO)" << endl;
-		}
-
-
-
 		else {
 			throw  string("OutputIEEE not yet implemented for wEI>wEO, send us a mail if you need it");
 		}
